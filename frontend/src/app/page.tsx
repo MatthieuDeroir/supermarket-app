c7bec71c8f11d3dd3f5b67286922d13a02ff585a
ziyad <ziyad.elhila@stormaudio.com>
2025-01-14 10:20:47 +0100

    TDEV700-3 🧹 clean (frontend): fix linter warnings

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 import styles from './page.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -74,4 +74,5 @@ export default function Home() {
       </footer>
     </div>
   );
-}
+};
+export default Home;
