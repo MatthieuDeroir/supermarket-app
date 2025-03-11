dbaad1d9ad9cd6456d540437ae6ea5b97b304bcb
ziyad <ziyad.elhila@stormaudio.com>
2025-03-11 11:38:25 +0100

    TDEV700-26 🚀 feat (frontend): protection des routes selon les roles

--- frontend/src/common/components/ProtectedRoute.tsx
+++ frontend/src/common/components/ProtectedRoute.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from 'next/router';
-import useAuth from '../hooks/useAuth';
-import { ReactNode, useEffect } from 'react';
+import useAuth from '@common/hooks/useAuth';
+import usePermissions from '@common/hooks/usePermissions';
+import permissions from '@common/defs/types/permissions';
+import { ReactNode, useEffect, useMemo } from 'react';
 
 interface ProtectedRouteProps {
   children: ReactNode;
@@ -9,18 +11,41 @@ interface ProtectedRouteProps {
 const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { user, initialized } = useAuth();
   const router = useRouter();
+  const { isAdmin, isManager, isUser } = usePermissions();
+
+  // Déterminer les routes accessibles en fonction du rôle de l'utilisateur
+  const accessibleRoutes = useMemo(() => {
+    if (isAdmin) {
+      return permissions.admin.items.flatMap((item) =>
+        item.itemLink ? [item.itemLink] : item.sousItems?.map((sub) => sub.itemLink) || [],
+      );
+    }
+    if (isManager) {
+      return permissions.manager.items.flatMap((item) =>
+        item.itemLink ? [item.itemLink] : item.sousItems?.map((sub) => sub.itemLink) || [],
+      );
+    }
+    if (isUser) {
+      return permissions.user.items.flatMap((item) =>
+        item.itemLink ? [item.itemLink] : item.sousItems?.map((sub) => sub.itemLink) || [],
+      );
+    }
+    return [];
+  }, [isAdmin, isManager, isUser]);
 
   useEffect(() => {
     if (initialized && !user) {
       router.push('/login');
+    } else if (initialized && user && !accessibleRoutes.includes(router.pathname)) {
+      router.push('/404'); // Rediriger vers une page d'erreur si l'utilisateur n'a pas accès
     }
-  }, [initialized, user, router]);
+  }, [initialized, user, router, accessibleRoutes]);
 
   if (!initialized) {
     return <div>Loading...</div>;
   }
 
-  if (!user) {
+  if (!user || !accessibleRoutes.includes(router.pathname)) {
     return null;
   }
 
